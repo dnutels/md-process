d9dade0a918dd02aa014869d83d37c19438215ba
Dmitry Nutels <dnutels@gmail.com>
2016-02-04 02:57:26 +0200

    Simple implementation, no Gulp

--- test/data/test.md
+++ test/data/test.md
@@ -1,7 +1,7 @@
 ---
 title: Server-side Rendering with React, Node and Express
-notebook: SSR With React - Revisions
 layout: article
+tags: react, ssr, node, express
 ---
 
 # Universal Web Applications with React, Node and Express
@@ -9,3 +9,14 @@ layout: article
 **Web applications are everywhere.**
 
 There is no official definition, but we've made the distinction: *web applications* are highly interactive, dynamic and performant, while *web sites* are informational and less transient. This **very rough** categorization provides us with a starting point, from which to apply development and design patterns.
+
+```perl
+#!/usr/local/bin/perl
+print "Content-type: text/html\n\n";
+print "<html>\n";
+print "<head><title>Perl - Hello, world!</title></head>\n";
+print "<body>\n";
+print "<h1>Hello, world!</h1>\n";
+print "</body>\n";
+print "</html>\n";
+```
--- test/main.specs.js
+++ test/main.specs.js
@@ -1,44 +1,131 @@
 /* eslint no-unused-expressions: 0 */
+import PATH from 'path';
 import FS from 'fs';
+import STREAM from 'stream';
+import VFile from 'vinyl';
+import through from 'through2';
 
 import {expect} from 'chai';
-// import md from '../lib/process';
-import VFS from 'vinyl-fs';
-import through from 'through2';
-import yamlFM from 'front-matter';
 
-const fm = through.obj(function(file, enc, done) {
-    if (file.isNull() || file.isStream()) {
-        return done(null, file);
-    }            
+import md, {processDiscrete} from '../lib/process';
 
-    const fileContent = file.contents.toString();
-    const meta = yamlFM(fileContent);
+const producer = function(options) {
+    const stream = new STREAM.Readable({objectMode: true});
 
-    this.push(file);
-    done();
-});
+    stream._read = function() {
+        this.push(new VFile(options));
+        this.push(null);
+    };
 
-const md = through.obj(function(file, enc, done) {
-    if (file.isNull() || file.isStream()) {
-        return done(null, file);
-    }            
+    return stream;
+};
 
-    const fileContent = file.contents.toString();
-    console.log(typeof fileContent);
-    const content = fileContent.replace(/---(.|\n)*?---/, '');
-    console.log(content);
+const tester = function(test) {
+    return through.obj(function(file, enc, done) {    
+        test(file);
+        done();
+    });  
+};
 
-    this.push(file);
-    done();
-});
+// Defined here to avoid incorrect indentation. Do not move.
+const metaExample = `---
+title: single-line title
+layout: article
+tags: [tag1, tag2, tag3]
+---
+`;
+
+const emptyMetaExample = '';
+
+const simpleExample = `# Heading
+`;
+
+const simpleResult = `<h1 id="heading">Heading</h1>
+`;
 
 describe('md-process', function() {
-    context('can receive', function() {
-        it('markdown file stream', function() {
-            VFS.src('./test/data/test.md')
-                .pipe(fm)
-                .pipe(md);
+    context('when imported as default', function() {        
+        it('is a function that returns a Stream', function() {
+            expect(md).to.be.a('function');
+            expect(md()).to.be.instanceOf(STREAM);
+        });
+        
+        context('and given a Vinyl file', function() {
+            it('which is empty - throws an error', function() {
+                producer({})
+                    .pipe(md())
+                    .pipe(tester(({meta, file}) => {
+                        const parts = PATH.parse(file.path);
+                        expect(parts).to.eql({ 
+                            root: '',
+                            dir: 'test',
+                            base: 'test.html',
+                            ext: '.html',
+                            name: 'test' 
+                        });
+                        done();
+                    }));
+            });
+
+            it('with relative path - returns Vinyl HTML file with correct corresponding path', function(done) {
+                producer({path: 'test/test', contents: new Buffer('')})
+                    .pipe(md())
+                    .pipe(tester(({meta, file}) => {
+                        const parts = PATH.parse(file.path);
+                        expect(parts).to.eql({ 
+                            root: '',
+                            dir: 'test',
+                            base: 'test.html',
+                            ext: '.html',
+                            name: 'test' 
+                        });
+                        done();
+                    }));
+            });
+        });
+
+        context('and given a file with non-empty YAML Front Matter', function() {
+            it('returns correct meta-information', function(done) {
+                producer({path: 'test/test', contents: new Buffer(metaExample)})
+                    .pipe(md())
+                    .pipe(tester(({meta}) => {
+                        expect(meta).to.eql({
+                            title: 'single-line title',
+                            layout: 'article',
+                            tags: ['tag1', 'tag2', 'tag3']
+                        });
+                        done();
+                    }));
+            });
+        });
+
+        context('and given a file with empty YAML Front Matter', function() {
+            it('returns empty meta-information', function(done) {
+                producer({path: 'test/test', contents: new Buffer(emptyMetaExample)})
+                    .pipe(md())
+                    .pipe(tester(({meta}) => {
+                        expect(meta).to.eql({});
+                        done();
+                    }));
+            });
+        });
+
+        context('and given a Vinyl with simple Markdown contents', function() {
+            it('returns Vinyl file with corresponding HTML content', function(done) {
+                producer({path: 'test/test', contents: new Buffer(simpleExample)})
+                    .pipe(md())
+                    .pipe(tester(({meta, file}) => {
+                        expect(file.contents.toString()).to.equal(simpleResult);
+                        done();
+                    }));
+            });
         });
     });
+
+    context('when imported as "processDiscrete"', function() {        
+        it('is a function that returns a Stream', function() {
+            expect(processDiscrete).to.be.a('function');
+        });
+    });
+
 });
